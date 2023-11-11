3d7f6813ceac1d47b3b4459250aad32b04444442
Idhibhat Pankam <marginpankam@gmail.com>
2023-11-11 14:59:54 +0700

    remove methods in user

--- MyGraderList/backend/user/user_grpc.pb.go
+++ MyGraderList/backend/user/user_grpc.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // - protoc-gen-go-grpc v1.3.0
 // - protoc             v4.24.4
-// source: MyGraderList/backend/user/user.proto
+// source: mygraderlist/backend/user/user.proto
 
 package user
 
@@ -19,24 +19,20 @@ import (
 const _ = grpc.SupportPackageIsVersion7
 
 const (
-	UserService_FindOne_FullMethodName     = "/user.UserService/FindOne"
 	UserService_FindByEmail_FullMethodName = "/user.UserService/FindByEmail"
 	UserService_Create_FullMethodName      = "/user.UserService/Create"
 	UserService_Update_FullMethodName      = "/user.UserService/Update"
 	UserService_Delete_FullMethodName      = "/user.UserService/Delete"
-	UserService_Verify_FullMethodName      = "/user.UserService/Verify"
 )
 
 // UserServiceClient is the client API for UserService service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type UserServiceClient interface {
-	FindOne(ctx context.Context, in *FindOneUserRequest, opts ...grpc.CallOption) (*FindOneUserResponse, error)
 	FindByEmail(ctx context.Context, in *FindByEmailUserRequest, opts ...grpc.CallOption) (*FindByEmailUserResponse, error)
 	Create(ctx context.Context, in *CreateUserRequest, opts ...grpc.CallOption) (*CreateUserResponse, error)
 	Update(ctx context.Context, in *UpdateUserRequest, opts ...grpc.CallOption) (*UpdateUserResponse, error)
 	Delete(ctx context.Context, in *DeleteUserRequest, opts ...grpc.CallOption) (*DeleteUserResponse, error)
-	Verify(ctx context.Context, in *VerifyUserRequest, opts ...grpc.CallOption) (*VerifyUserResponse, error)
 }
 
 type userServiceClient struct {
@@ -45,15 +41,6 @@ type userServiceClient struct {
 
 func NewUserServiceClient(cc grpc.ClientConnInterface) UserServiceClient {
 	return &userServiceClient{cc}
-}
-
-func (c *userServiceClient) FindOne(ctx context.Context, in *FindOneUserRequest, opts ...grpc.CallOption) (*FindOneUserResponse, error) {
-	out := new(FindOneUserResponse)
-	err := c.cc.Invoke(ctx, UserService_FindOne_FullMethodName, in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
 }
 
 func (c *userServiceClient) FindByEmail(ctx context.Context, in *FindByEmailUserRequest, opts ...grpc.CallOption) (*FindByEmailUserResponse, error) {
@@ -92,25 +79,14 @@ func (c *userServiceClient) Delete(ctx context.Context, in *DeleteUserRequest, o
 	return out, nil
 }
 
-func (c *userServiceClient) Verify(ctx context.Context, in *VerifyUserRequest, opts ...grpc.CallOption) (*VerifyUserResponse, error) {
-	out := new(VerifyUserResponse)
-	err := c.cc.Invoke(ctx, UserService_Verify_FullMethodName, in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
 // UserServiceServer is the server API for UserService service.
 // All implementations must embed UnimplementedUserServiceServer
 // for forward compatibility
 type UserServiceServer interface {
-	FindOne(context.Context, *FindOneUserRequest) (*FindOneUserResponse, error)
 	FindByEmail(context.Context, *FindByEmailUserRequest) (*FindByEmailUserResponse, error)
 	Create(context.Context, *CreateUserRequest) (*CreateUserResponse, error)
 	Update(context.Context, *UpdateUserRequest) (*UpdateUserResponse, error)
 	Delete(context.Context, *DeleteUserRequest) (*DeleteUserResponse, error)
-	Verify(context.Context, *VerifyUserRequest) (*VerifyUserResponse, error)
 	mustEmbedUnimplementedUserServiceServer()
 }
 
@@ -118,9 +94,6 @@ type UserServiceServer interface {
 type UnimplementedUserServiceServer struct {
 }
 
-func (UnimplementedUserServiceServer) FindOne(context.Context, *FindOneUserRequest) (*FindOneUserResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method FindOne not implemented")
-}
 func (UnimplementedUserServiceServer) FindByEmail(context.Context, *FindByEmailUserRequest) (*FindByEmailUserResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method FindByEmail not implemented")
 }
@@ -133,9 +106,6 @@ func (UnimplementedUserServiceServer) Update(context.Context, *UpdateUserRequest
 func (UnimplementedUserServiceServer) Delete(context.Context, *DeleteUserRequest) (*DeleteUserResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Delete not implemented")
 }
-func (UnimplementedUserServiceServer) Verify(context.Context, *VerifyUserRequest) (*VerifyUserResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method Verify not implemented")
-}
 func (UnimplementedUserServiceServer) mustEmbedUnimplementedUserServiceServer() {}
 
 // UnsafeUserServiceServer may be embedded to opt out of forward compatibility for this service.
@@ -147,24 +117,6 @@ type UnsafeUserServiceServer interface {
 
 func RegisterUserServiceServer(s grpc.ServiceRegistrar, srv UserServiceServer) {
 	s.RegisterService(&UserService_ServiceDesc, srv)
-}
-
-func _UserService_FindOne_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(FindOneUserRequest)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(UserServiceServer).FindOne(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: UserService_FindOne_FullMethodName,
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(UserServiceServer).FindOne(ctx, req.(*FindOneUserRequest))
-	}
-	return interceptor(ctx, in, info, handler)
 }
 
 func _UserService_FindByEmail_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
@@ -239,24 +191,6 @@ func _UserService_Delete_Handler(srv interface{}, ctx context.Context, dec func(
 	return interceptor(ctx, in, info, handler)
 }
 
-func _UserService_Verify_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(VerifyUserRequest)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(UserServiceServer).Verify(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: UserService_Verify_FullMethodName,
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(UserServiceServer).Verify(ctx, req.(*VerifyUserRequest))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
 // UserService_ServiceDesc is the grpc.ServiceDesc for UserService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -264,10 +198,6 @@ var UserService_ServiceDesc = grpc.ServiceDesc{
 	ServiceName: "user.UserService",
 	HandlerType: (*UserServiceServer)(nil),
 	Methods: []grpc.MethodDesc{
-		{
-			MethodName: "FindOne",
-			Handler:    _UserService_FindOne_Handler,
-		},
 		{
 			MethodName: "FindByEmail",
 			Handler:    _UserService_FindByEmail_Handler,
@@ -284,11 +214,7 @@ var UserService_ServiceDesc = grpc.ServiceDesc{
 			MethodName: "Delete",
 			Handler:    _UserService_Delete_Handler,
 		},
-		{
-			MethodName: "Verify",
-			Handler:    _UserService_Verify_Handler,
-		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: "MyGraderList/backend/user/user.proto",
+	Metadata: "mygraderlist/backend/user/user.proto",
 }
